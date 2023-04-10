922a4db366376091a0780e5a7bdad3fffac3fe23
İbrahim Alkan Arpat <alkanarpat@hotmail.com>
2023-04-11 00:02:40 +0300

    202304110002

---
+++ ProjeBlog/Migrations/20230405155345_20230405_1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjeBlog.Migrations
+{
+    public partial class _20230405_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
