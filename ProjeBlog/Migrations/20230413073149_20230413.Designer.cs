e30d72bd23555f9948cb21134bb433ffc9da6a30
alkan <alkanarpat@hotmail.com>
2023-04-13 17:28:37 +0300

    20230413/2

--- ProjeBlog/Migrations/20230413073149_20230413.Designer.cs
+++ ProjeBlog/Migrations/20230413073149_20230413.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjeBlog.Context;
 
 namespace ProjeBlog.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230413073149_20230413")]
+    partial class _20230413
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
