a43c59364f2b54ad84c590abdb5aac36c089aed0
alkan <alkanarpat@hotmail.com>
2023-04-14 09:30:17 +0300

    20230414, Controllerdan kullanıcılar listeye alındı.

---
+++ ProjeBlog/Areas/Management/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjeBlog.Context;
+using ProjeBlog.Models;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace ProjeBlog.Areas.Management.Controllers
+{
+    [Area("Management")]
+    public class UserController : Controller
+    {
+        MyDbContext _db;
+        public UserController(MyDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult UserList()
+        {
+            List<AppUser> users = _db.Users.ToList();
+            return View(users);
+        }
+    }
+}
